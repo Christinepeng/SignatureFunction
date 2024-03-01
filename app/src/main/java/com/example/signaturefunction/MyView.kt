7856813bc6fe963271bcb2de316852d3b6905fa0
Yellow Bear <percussionpeng@gmail.com>
2024-02-29 19:03:02 -0800

    draw separate lines.

--- app/src/main/java/com/example/signaturefunction/MyView.kt
+++ app/src/main/java/com/example/signaturefunction/MyView.kt
@@ -13,12 +13,10 @@ import java.util.*
 import kotlin.collections.HashMap
 
 class MyView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
-//    private val lines = LinkedList<LinkedList<HashMap<String, Float>>>()
-    private val line = LinkedList<HashMap<String, Float>>()
+    private val lines = LinkedList<LinkedList<HashMap<String, Float>>>()
     private val paint = Paint()
 
     init {
-        Log.v("zz", "set paint")
         paint.setColor(Color.BLUE)
         paint.strokeWidth= 9F
     }
@@ -26,42 +24,27 @@ class MyView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
         super.onDraw(canvas)
         Log.v("zz", "onDraw()")
 
-//        lines.forEach {
+        lines.forEach { line ->
             for (i in 1..<line.size) {
                 val p0 = line[i - 1]
                 val p1 = line[i]
                 canvas.drawLine(p0["x"]!!, p0["y"]!!, p1["x"]!!, p1["y"]!!, paint)
             }
-//        }
-
+        }
     }
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent): Boolean {
-//        Log.v("zz", "set paint in onTouchEvent()")
-//        paint.setColor(Color.BLUE)
-//        paint.strokeWidth = 8F
         when (event.action) {
             MotionEvent.ACTION_DOWN -> setFirstPoint(event)
             MotionEvent.ACTION_MOVE -> setMovePoint(event)
         }
-
-//        if (event.action == MotionEvent.ACTION_DOWN) {
-//            Log.v("zz", "down")
-//            setFirstPoint(event)
-//        } else if (event.action == MotionEvent.ACTION_UP) {
-//            Log.v("zz", "up")
-//        } else if (event.action == MotionEvent.ACTION_MOVE) {
-//            Log.v("zz", "move")
-//            setMovePoint(event)
-//        }
-//        val ret = super.onTouchEvent(event)
         return true
     }
 
     private fun setFirstPoint(event: MotionEvent) {
         Log.v("zz", "setFirstPoint()")
-//        val line = LinkedList<HashMap<String, Float>>()
+        val line = LinkedList<HashMap<String, Float>>()
 
         val ex = event.x
         val ey = event.y
@@ -70,7 +53,7 @@ class MyView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
         point["y"] = ey
         line.add(point)
 
-//        lines.add(line)
+        lines.add(line)
     }
 
     private fun setMovePoint(event: MotionEvent) {
@@ -80,8 +63,7 @@ class MyView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
         val point = HashMap<String, Float>()
         point["x"] = ex
         point["y"] = ey
-        line.add(point)
-//        lines.last.add(point)
+        lines.last.add(point)
 
         invalidate()
     }
